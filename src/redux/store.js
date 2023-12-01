780e39be0051e36a55a70603fd61ccf07c5069d0
Chu Đức Hùng <chuduchung@MacBook-Pro.local>
2023-12-01 17:15:49 +0700

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,24 @@
+// import {createStore} from 'redux'
+// import rootReducer from './reducer'
+// import { composeWithDevTools } from 'redux-devtools-extension'
+
+// const composedEnhancers = composeWithDevTools()
+
+// const store = createStore(rootReducer, composedEnhancers)
+
+// export default store
+
+
+import filtersSlice from "../components/Filters/filtersSlice"
+import todoListSlice from "../components/TodoList/todoListSlice"
+
+import {configureStore} from '@reduxjs/toolkit'
+
+const store = configureStore({
+    reducer:{
+        filters: filtersSlice.reducer,
+        todoList: todoListSlice.reducer
+    }
+})
+
+export default store
